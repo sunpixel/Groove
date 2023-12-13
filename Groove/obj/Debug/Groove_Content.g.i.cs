4f1310572c177cf8d3f433b67cf5cd593ca42cbe
SunPixel <tarakinnikita@gmail.com>
2023-12-14 01:04:25 +0300

    UI updates

--- Groove/obj/Debug/Groove_Content.g.i.cs
+++ Groove/obj/Debug/Groove_Content.g.i.cs
@@ -8,10 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/forward.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/next.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/pause.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/play.png")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/speaker.png")]
 
 
